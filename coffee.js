d1f98fe25b8acf14d9653709b1e39ba31dcf5cf1
k190304 <102511379+k190304@users.noreply.github.com>
2024-08-03 08:57:52 +0530

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,40 @@
+status = "";
+
+function preload()
+{
+    img = loadImage('coffee.jpeg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResults()
+{
+    if(error)
+        {
+            console.log(error);
+        }
+        console.log(results);
+}
+
+function back()
+{
+    window.location('index.html');
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function book()
+{
+    window.location('book.html');
+}
+
+function laptop()
+{
+    window.location('laptop.html');
+}
+
+function cat()
+{
+    window.location('cat.html');
+}
+
+function coffee()
+{
+    window.location('coffee.html');
+}
